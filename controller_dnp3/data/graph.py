655ef691c05bc2ef64bbb1aea79deb47dbe019ba
Juan Mejia Rojas <juanro.mejia@gmail.com>
2016-08-11 17:38:43 +0200

    attacker data

--- controller_dnp3/data/graph.py
+++ controller_dnp3/data/graph.py
@@ -1,8 +1,16 @@
-import matplotlib.pyplot as plt
+import plotly.plotly as py
+import plotly.graph_objs as go
 import numpy as np
 
-gtotal = np.loadtxt("gtotal_untouch_replayattack1470756838049", usecols=(0,1))
-distance = np.loadtxt("gtotal_untouch_replayattack1470756838049", usecols=(0,2))
-plt.plot(gtotal)
-#plt.plot(distance)
-plt.show()
+gtotal = np.loadtxt("cyberphysicalattack1", usecols=(1,))
+time = np.loadtxt("cyberphysicalattack1", usecols=(0,))
+
+trace = go.Scatter(
+	x = time,
+	y = gtotal
+)
+data = [trace]
+py.sign_in('juanro', 'bndxzextmu')
+py.iplot(data, filename='basic-line')
+
+py.iplot(data, filename='basic-line')
